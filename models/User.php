1d28faff2eada0542bdc7bccebc68cc2bebe4811
unknown <dimas.karim@DIMAS_JABBAR.dwp.co.id>
2015-07-29 14:57:10 +0700

    initial

---
+++ models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'required'],
+            [['id'], 'integer'],
+            [['created_at', 'updated_at'], 'default',
+              'value'=>new Expression('NOW()'),
+                ],
+            [['username'], 'string', 'max' => 25],
+            [['password'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
